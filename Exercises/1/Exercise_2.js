cfcc40093c1cd981a4a77dadd4ed5d530cad1da8
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-02-10 21:37:57 +0100

    add comments to clarify the objectives of Exercise 2 and Exercise 3 implementations

--- Exercises/1/Exercise_2.js
+++ Exercises/1/Exercise_2.js
@@ -1,3 +1,9 @@
+// Write a for loop that:
+
+// 1. Creates an array of the first 10 even numbers (2, 4, 6, 8, ...)
+// 2. The calculates the sum of those numbers in a second loop
+// 3. Finally, prints the array and the sum
+
 const forLoop = () => {
     const evenNumbers = [];
     for (let i = 2; i <= 20; i += 2) {
